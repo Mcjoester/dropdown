3f7975107ba6a3a12c27927d122f01c0b9dfc3f4
Jose <jpelayo53@gmail.com>
2025-03-26 12:31:35 -0700

    set up dropdowns with reusable functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import "./style.css";
-// EX: import { greeting } from "./greeting.js";
 
-// console.log(greeting);
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach((dropdown) => {
+        const btn = dropdown.querySelector('.btn');
+        const content = dropdown.querySelector('.dropdown-content');
+        const menuItems = dropdown.querySelectorAll('.menu-item');
+
+        btn.addEventListener('click', () => {
+            content.classList.toggle('active');
+        })
+
+        menuItems.forEach((item) => {
+            item.addEventListener('click', () => {
+                content.classList.toggle('active');
+            })
+        })
+
+        document.addEventListener('click', (event) => {
+            if (!dropdown.contains(event.target)) {
+                content.classList.remove('active');
+
+            }
+        })
+
+    })
+})
